f3b5b81990bd915cdd4da49013fa337232a0567f
yayachere <chereyaya16@gmail.com>
2025-07-06 16:47:41 +0300

    update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <ErrorBoundary>{children}</ErrorBoundary>
